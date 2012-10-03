1e8a29dcb8aa0ced1d7b8463b352414ab7199ab5
1000000000 <lthyde@gmail.com>
2012-10-02 23:27:44 -0400

    CHECK FOR TYPOS!! Added new class utility to have methods which include long equations or other math stuff and added a class which returns the two solutions for the collision finding equation

---
+++ src/com/labprogramming/gravity/MathUtil.java
@@ -0,0 +1,36 @@
+package com.labprogramming.gravity;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class MathUtil {
+	
+	public static final double SQRT_2 = sqrt(2);
+	
+	private MathUtil() {}
+	
+	public static double[] getTimeTillCollision(Body p, Body q) {
+		double r_p = p.getRadius();
+		double r_q = q.getRadius();
+		double x_p_o = p.getX();
+		double x_q_o = q.getX();
+		double v_p_x_o = p.getVelX();
+		double v_q_x_o = q.getVelX();
+		double a_p_x = p.getAcelX();
+		double a_q_x = q.getAcelX();
+		double y_p_o = p.getY();
+		double y_q_o = q.getY();
+		double v_p_y_o = p.getVelY();
+		double v_q_y_o = q.getVelY();
+		double a_p_y = p.getAcelY();
+		double a_q_y = q.getAcelY();
+		return new double[] {
+				SQRT_2/2 * sqrt(4* (sqrt(sqr((a_p_x-a_q_x)*(x_p_o-x_q_o)+(a_p_y-a_q_y)*(y_p_o-y_q_o)) + (sqr(a_p_x-a_q_x) + sqr(a_p_y-a_q_y))*(sqr(r_p+r_q) - sqr(x_p_o-x_q_o) - sqr(y_p_o-y_q_o))) - ((a_p_x-a_q_x)*(x_p_o-x_q_o) + (a_p_y-a_q_y)*(y_p_o-y_q_o)))/((a_p_x-a_q_x) + (a_p_y-a_q_y))),
+				SQRT_2/2 * sqrt(4*(-sqrt(sqr((a_p_x-a_q_x)*(x_p_o-x_q_o)+(a_p_y-a_q_y)*(y_p_o-y_q_o)) + (sqr(a_p_x-a_q_x) + sqr(a_p_y-a_q_y))*(sqr(r_p+r_q) - sqr(x_p_o-x_q_o) - sqr(y_p_o-y_q_o))) - ((a_p_x-a_q_x)*(x_p_o-x_q_o) + (a_p_y-a_q_y)*(y_p_o-y_q_o)))/((a_p_x-a_q_x) + (a_p_y-a_q_y)))
+		};
+	}
+	
+	public static double sqr(double root) {
+		return pow(root, 2);
+	}
+}
