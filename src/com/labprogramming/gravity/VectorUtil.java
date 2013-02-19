0c13eaf999f58477438a592b694b57b0b4fc624f
1000000000 <lthyde@gmail.com>
2013-02-19 15:24:50 -0500

    Made VectorUtil work better with vectors as an angle and magnitude

--- src/com/labprogramming/gravity/VectorUtil.java
+++ src/com/labprogramming/gravity/VectorUtil.java
@@ -1,7 +1,12 @@
 package com.labprogramming.gravity;
 
+import static java.lang.Math.atan2;
+import static java.lang.Math.hypot;
+
 class VectorUtil {
 
+	private final double mag;
+	private final double angle;
 	private final double xMag;
 	private final double yMag;
 
@@ -9,11 +14,15 @@ class VectorUtil {
 		double scaleFactor = hypot != 0 ? force / hypot : 0;
 		xMag = scaleFactor * xDest;
 		yMag = scaleFactor * yDest;
+		mag = force;
+		angle = atan2(yMag, xMag);
 	}
 
 	public VectorUtil(double newXMag, double newYMag) {
 		xMag = newXMag;
 		yMag = newYMag;
+		mag = hypot(xMag, yMag);
+		angle = atan2(yMag, xMag);
 	}
 
 	/**
@@ -28,5 +37,17 @@ class VectorUtil {
 	 */
 	public double getYMag() {
 		return yMag;
+	}
+	
+	public double getMag() {
+		return mag;
+	}
+	
+	public double getDirection() {
+		return angle;
+	}
+	
+	public static VectorUtil scaleMagnitude(VectorUtil vector, double newMag) {
+		return new VectorUtil(newMag, vector.mag, vector.xMag, vector.yMag);
 	}
 }
